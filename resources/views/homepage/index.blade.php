ea62f2d3a6a3919e404463b113dbc7414552c1f1
fathurwalkers <fathurwalkers44@gmail.com>
2020-11-19 22:07:04 +0800

    sdsdadad

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -91,10 +91,39 @@
 
 {{-- Main Content Here  --}}
 
-<div class="container-fluid my-5 py-5">
-  <h1>
-    TEST
-  </h1>
+<div class="container my-5 py-5 mx-auto d-inline-flex">
+
+{{-- <div class="col-lg-12 mx-auto d-inline-flex"> --}}
+
+  <div class="col-sm-4">
+    <div class="card" style="width: 18rem;">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXp3DxP80ArpRzsB0XWBG9Ow5GeuefbLrUHw&usqp=CAU" class="card-img-top img-fluid" width="250px" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-4">
+    <div class="card" style="width: 18rem;">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXp3DxP80ArpRzsB0XWBG9Ow5GeuefbLrUHw&usqp=CAU" class="card-img-top img-fluid" width="250px" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-4">
+    <div class="card" style="width: 18rem;">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXp3DxP80ArpRzsB0XWBG9Ow5GeuefbLrUHw&usqp=CAU" class="card-img-top img-fluid" width="250px" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </div>
+
+{{-- </div> --}}
+
 </div>
 
 {{-- End of Main Content  --}}
